8225a1e195eb1c89f0b81f495069cd6db34bfbda
wayne868 <jaggernauthw@gmail.com>
2025-04-28 11:40:37 -0400

    Set up database and installed apps

--- translations/translation_app/models.py
+++ translations/translation_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TextEntry (models.Model):
+    english_text = models.TextField()
+    french_text = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.english_text[:50] #Display first 50 characters
