94debaffa91ffa1fea4f1cf2b92b971e18bc276c
wayne868 <jaggernauthw@gmail.com>
2025-04-28 14:00:50 -0400

    Psst

--- translations/translations/settings.py
+++ translations/translations/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+=zff3djl(0k$)w35!ptw15ed@=y30y9nk&c=1(32#j_ni4cl%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'translations.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'translator',
-        'USER': 'root',
-        'PASSWORD': 'Molesworth123',
-        'HOST': '192.168.1.82',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
